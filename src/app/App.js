f9c6679f6c67139ca537fbe9bcc9f0065ce82b31
Rishikesh <rishikesh@intelligaia.com>
2024-10-24 02:05:37 +0530

    Updated build

--- src/app/App.js
+++ src/app/App.js
@@ -1,35 +1,40 @@
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, StaticRouter, Switch, Route } from "react-router-dom";
 import { HelmetMeta } from "./HelmetMeta";
 import { ThemeProvider } from "../components/theme/ThemeProvider";
 import { CssBaseline } from "@material-ui/core";
-
 import { Home } from "../pages/Home";
 
 // Lazy-loaded components
-const Resume = lazy(() => import("../pages/ResumePage").then(module => ({ default: module.ResumePage})));
-const PageNotFound = lazy(() => import("../pages/PageNotFound").then(module => ({ default: module.PageNotFound })));
-const Works = lazy(() => import("../pages/ProjectsPage").then(module => ({ default: module.ProjectsPage })));
+const Resume = lazy(() =>
+  import("../pages/ResumePage").then(module => ({ default: module.ResumePage }))
+);
+const PageNotFound = lazy(() =>
+  import("../pages/PageNotFound").then(module => ({ default: module.PageNotFound }))
+);
+const Works = lazy(() =>
+  import("../pages/ProjectsPage").then(module => ({ default: module.ProjectsPage }))
+);
 
-
+// Conditionally set the Router based on environment
+const Router = process.env.REACT_APP_SNAPSHOT === 'true' ? StaticRouter : BrowserRouter;
 
 export const App = () => {
-
-    return (
-      <ThemeProvider>
-        <CssBaseline />
-        <Router>
-          <HelmetMeta />
-          {/* Wrap lazy-loaded routes with Suspense */}
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/resume" component={Resume} />
-              <Route path="/projects" component={Works} />
-              <Route path="*" component={PageNotFound} />
-            </Switch>
-          </Suspense>
-        </Router>
-      </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <CssBaseline />
+      <Router basename="/">
+        <HelmetMeta />
+        {/* Wrap lazy-loaded routes with Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/projects" component={Works} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </ThemeProvider>
+  );
 };
